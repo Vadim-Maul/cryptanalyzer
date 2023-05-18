f7afbd3b63adea32b1a5ce7d4358bf1100ef9569
Vadim-Maul <93133379+Vadim-Maul@users.noreply.github.com>
2023-05-18 16:21:00 +0200

    Update DefaultTextPath.java

--- src/main/java/com/javarush/cryptanalyzer/maul/constants/DefaultTextPath.java
+++ src/main/java/com/javarush/cryptanalyzer/maul/constants/DefaultTextPath.java
@@ -4,5 +4,7 @@ import java.nio.file.Path;
 
 public class DefaultTextPath {
 
-    public static final Path DEFAULT_TEXT_PATH = Path.of("text.txt");
+    public static final Path DEFAULT_TEXT_PATH_INPUT = Path.of("input.txt");
+    public static final Path DEFAULT_TEXT_PATH_ENCODED = Path.of("encoded.txt");
+    public static final Path DEFAULT_TEXT_PATH_OUTPUT = Path.of("output.txt");
 }
